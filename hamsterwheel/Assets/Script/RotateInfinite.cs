c957fdfb2a6536b9c38be21d111ff25453dac7e3
Tania A <ithiliel@gmail.com>
2020-10-03 22:16:42 +0200

    Hamster animator fsm + placeholder graphics (except for failure condition) Wheel rotation Test inputs

---
+++ hamsterwheel/Assets/Script/RotateInfinite.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateInfinite : MonoBehaviour
+{
+
+    public float DegreesPerSecond = 360;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, DegreesPerSecond * Time.deltaTime);
+        
+    }
+}
